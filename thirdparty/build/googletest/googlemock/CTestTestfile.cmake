5e1c99e3e853a2ef7ada3230da76cf7fbb69ee18
Benjamin Zamour <benjamin.zamour@gmail.com>
2021-02-02 16:43:14 +0100

    remove thirdparty build directory from repo

--- thirdparty/build/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ben/Bureau/dev/FDFramework/thirdparty/googletest/googlemock
-# Build directory: /home/ben/Bureau/dev/FDFramework/thirdparty/build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
